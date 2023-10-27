d36db3f9f44fa47fa82abc34b91f8631aadc3788
Sami Nagahama <nagahamasami@gmail.com>
2023-10-27 19:02:04 -0300

    Alteração do placar e do config para adaptar às regras de futebol

--- app/src/main/java/ufc/smd/esqueleto_placar/ConfigActivity.kt
+++ app/src/main/java/ufc/smd/esqueleto_placar/ConfigActivity.kt
@@ -12,55 +12,51 @@ import android.widget.Switch
 import data.Placar
 
 class ConfigActivity : AppCompatActivity() {
-    var placar: Placar= Placar("Jogo sem Config","0x0", "20/05/20 10h",false)
+    var placar: Placar = Placar("Jogo sem Config", "0 - 0", "20/05/20 10h", false) // Adjusted score format
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
-       // placar= getIntent().getExtras()?.getSerializable("placar") as Placar
-        //Log.v("PDM22",placar.nome_partida)
-        //Log.v("PDM22",placar.has_timer.toString())
-
 
         openConfig()
         initInterface()
-
     }
-    fun saveConfig(){
+
+    fun saveConfig() {
         val sharedFilename = "configPlacar"
-        val sp:SharedPreferences = getSharedPreferences(sharedFilename,Context.MODE_PRIVATE)
+        val sp: SharedPreferences = getSharedPreferences(sharedFilename, Context.MODE_PRIVATE)
         var edShared = sp.edit()
 
-
-        edShared.putString("matchname",placar.nome_partida)
-        edShared.putBoolean("has_timer",placar.has_timer)
+        edShared.putString("matchname", placar.nome_partida)
+        edShared.putBoolean("has_timer", placar.has_timer)
         edShared.commit()
     }
-    fun openConfig()
-    {
+
+    fun openConfig() {
         val sharedFilename = "configPlacar"
-        val sp:SharedPreferences = getSharedPreferences(sharedFilename,Context.MODE_PRIVATE)
-        placar.nome_partida=sp.getString("matchname","Jogo Padrão").toString()
-        placar.has_timer=sp.getBoolean("has_timer",false)
-
+        val sp: SharedPreferences = getSharedPreferences(sharedFilename, Context.MODE_PRIVATE)
+        placar.nome_partida = sp.getString("matchname", "Jogo Padrão").toString()
+        placar.has_timer = sp.getBoolean("has_timer", false)
     }
-    fun initInterface(){
-        val tv= findViewById<EditText>(R.id.editTextGameName)
+
+    fun initInterface() {
+        val tv = findViewById<EditText>(R.id.editTextGameName)
         tv.setText(placar.nome_partida)
-        val sw= findViewById<Switch>(R.id.swTimer)
-        sw.isChecked=placar.has_timer
+        val sw = findViewById<Switch>(R.id.swTimer)
+        sw.isChecked = placar.has_timer
     }
 
-    fun updatePlacarConfig(){
-        val tv= findViewById<EditText>(R.id.editTextGameName)
-        val sw= findViewById<Switch>(R.id.swTimer)
-        placar.nome_partida= tv.text.toString()
-        placar.has_timer=sw.isChecked
+    fun updatePlacarConfig() {
+        val tv = findViewById<EditText>(R.id.editTextGameName)
+        val sw = findViewById<Switch>(R.id.swTimer)
+        placar.nome_partida = tv.text.toString()
+        placar.has_timer = sw.isChecked
     }
 
-    fun openPlacar(v: View){ //Executa ao click do Iniciar Jogo
-        updatePlacarConfig() //Pega da Interface e joga no placar
-        saveConfig() //Salva no Shared preferences
-        val intent = Intent(this, PlacarActivity::class.java).apply{
+    fun openPlacar(v: View) {
+        updatePlacarConfig() // Get data from the interface and update the placar
+        saveConfig() // Save to shared preferences
+        val intent = Intent(this, PlacarActivity::class.java).apply {
             putExtra("placar", placar)
         }
         startActivity(intent)
--- app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
+++ app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
@@ -7,11 +7,13 @@ import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.VibrationEffect
 import android.os.Vibrator
 
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.content.getSystemService
 import data.Placar
@@ -22,43 +24,91 @@ import java.io.ObjectOutputStream
 import java.nio.charset.StandardCharsets
 
 class PlacarActivity : AppCompatActivity() {
-    lateinit var placar:Placar
+    lateinit var placar: Placar
     lateinit var tvResultadoJogo: TextView
-    var game =0
+
+    var goalsTeamA = 0
+    var goalsTeamB = 0
+
+    private var elapsedTime = 0L
+    private val handler = Handler()
+    private lateinit var timerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placar)
-        placar= getIntent().getExtras()?.getSerializable("placar") as Placar
-        tvResultadoJogo= findViewById(R.id.tvPlacar)
-        //Mudar o nome da partida
-        val tvNomePartida=findViewById(R.id.tvNomePartida2) as TextView
-        tvNomePartida.text=placar.nome_partida
-        ultimoJogos()
-    }
 
-    fun alteraPlacar (v:View){
-        game++
-        if ((game % 2) != 0) {
-            placar.resultado = ""+game+" vs "+ (game-1)
-        }else{
-            placar.resultado = ""+(game-1)+" vs "+ (game-1)
-            vibrar(v)
+        placar = intent.extras?.getSerializable("placar") as Placar
+        tvResultadoJogo = findViewById(R.id.tvPlacar)
+        val tvNomePartida = findViewById<TextView>(R.id.tvNomePartida2)
+        tvNomePartida.text = placar.nome_partida
+
+        timerButton = findViewById(R.id.timerButton)
+        timerButton.setOnClickListener {
+            when {
+                elapsedTime < 45 * 60 * 1000 -> startFirstHalfTimer()
+                elapsedTime < (45 + 15) * 60 * 1000 -> startBreakTime()
+                else -> startSecondHalfTimer()
+            }
         }
-        tvResultadoJogo.text=placar.resultado
+
+        ultimoJogos()
+        updateScoreDisplay()
     }
 
+    fun addGoalTeamA(v: View) {
+        goalsTeamA++
+        updateScoreDisplay()
+        vibrar(v)
+    }
 
-    fun vibrar (v:View){
-        val buzzer = this.getSystemService<Vibrator>()
-         val pattern = longArrayOf(0, 200, 100, 300)
-         buzzer?.let {
-             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                 buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
-             } else {
-                 //deprecated in API 26
-                 buzzer.vibrate(pattern, -1)
-             }
-         }
+    fun addGoalTeamB(v: View) {
+        goalsTeamB++
+        updateScoreDisplay()
+        vibrar(v)
+    }
+
+    private fun updateScoreDisplay() {
+        placar.resultado = "$goalsTeamA - $goalsTeamB"
+        tvResultadoJogo.text = placar.resultado
+    }
+
+    private fun startFirstHalfTimer() {
+        handler.postDelayed(timerRunnable, 1000)
+    }
+
+    private fun startBreakTime() {
+        handler.postDelayed({
+            startSecondHalfTimer()
+        }, 15 * 60 * 1000)
+    }
+
+    private fun startSecondHalfTimer() {
+        handler.postDelayed(timerRunnable, 1000)
+    }
+
+    private val timerRunnable = object : Runnable {
+        override fun run() {
+            elapsedTime += 1000
+
+            if (elapsedTime < 45 * 60 * 1000 || (elapsedTime > (45 + 15) * 60 * 1000 && elapsedTime < (45 + 15 + 45) * 60 * 1000)) {
+                handler.postDelayed(this, 1000)
+            } else {
+                // Notify the end of a half or end of the match
+            }
+        }
+    }
+
+    fun vibrar(v: View) {
+        val buzzer = getSystemService<Vibrator>()
+        val pattern = longArrayOf(0, 200, 100, 300)
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                buzzer.vibrate(pattern, -1)
+            }
+        }
 
     }
 
@@ -110,6 +160,9 @@ class PlacarActivity : AppCompatActivity() {
             var prevPlacar:Placar = oos.readObject() as Placar
             Log.v("PDM22", "Jogo Salvo:"+ prevPlacar.resultado)
         }
-
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(timerRunnable)
     }
 }
